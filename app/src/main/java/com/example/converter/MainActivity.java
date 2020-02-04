77f78ec3bae27a9f07d33297e460eab211f2e5a7
Darshan <darshan.kodipalli@gmail.com>
2020-02-04 13:03:43 -0600

    Final Update

--- app/src/main/java/com/example/converter/MainActivity.java
+++ app/src/main/java/com/example/converter/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -29,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
     private TextView history;
     private String conversionHistory;
+    private TextView conversionHistoryHeadingText;
+    private TextView applicationHeading;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate: ");
@@ -44,23 +48,31 @@ public class MainActivity extends AppCompatActivity {
         history = findViewById(R.id.conversionHistory);
         conversionHistory = "";
         finalResult.setEnabled(false);
-        history.setEnabled(false);
+        history.setEnabled(true);
+        inputValue.setHint("Enter distance in Miles...");
         inputHeading.setTextColor(Color.parseColor("#FF000000"));
         outputHeading.setTextColor(Color.parseColor("#FF000000"));
         history.setMovementMethod(new ScrollingMovementMethod());
         history.setTextColor(Color.parseColor("#FF000000"));
+        conversionHistoryHeadingText = findViewById(R.id.conversionHistoryHeading);
+        conversionHistoryHeadingText.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        conversionHistoryHeadingText.setTextColor(Color.parseColor("#FF000000"));
+        applicationHeading = findViewById(R.id.applicationHeading);
+        applicationHeading.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
     }
 
     public void onClickingRadioButton(View view){
          radioInput = ((RadioButton) view).getText().toString();
         String textViewField = ((TextView) view).getText().toString();
         if(radioInput.equals("Kilometers to Miles")){
+            inputValue.setHint("Enter distance in Kilometers...");
             inputValue.setText("");
             finalResult.setText("");
             inputHeading.setText("Kilometers: ");
             outputHeading.setText("Miles:");
             buttonText.setText("Convert to Miles");
         }else {
+            inputValue.setHint("Enter distance in Miles...");
             inputValue.setText("");
             finalResult.setText("");
             inputHeading.setText("Miles: ");
@@ -74,22 +86,38 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "Radio Button ID: "+id);
         Log.d(TAG, "onConvertOption: "+R.id.mitokms);
         Log.d(TAG, "Input Value: "+inputValue.getText());
-        Double inputValueInNumber = Double.parseDouble(inputValue.getText().toString());
-        Double output;
-        if(radioInput.equals("Kilometers to Miles")){
-            Log.d(TAG, "Input in Kilometers: "+inputValueInNumber);
-            output = (inputValueInNumber)*0.621371;
-            output = Math.round(output*10.0)/10.0;
-            conversionHistory = System.getProperty("line.separator")+"     "+inputValueInNumber.toString()+"  Km ===>  "+output.toString()+"  Mi"+conversionHistory;
-            history.setText(conversionHistory);
-            finalResult.setText("  "+output.toString()+"   miles");
+        if(inputValue.getText().toString().equals("")){
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Invalid Input");
+            alert.setMessage("Invalid input entered. Please enter a valid input");
+            alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    inputValue.setText("");
+                    finalResult.setText("");
+                }
+            });
+            alert.create().show();
         }else {
-            Log.d(TAG, "Input in Miles: "+inputValueInNumber);
-            output = (inputValueInNumber)*1.60934;
-            output = Math.round(output*10.0)/10.0;
-            finalResult.setText("  "+output.toString()+"   kilometers");
-            conversionHistory =System.getProperty("line.separator")+"     "+inputValueInNumber.toString()+"  Mi  ===>  "+output.toString()+"  Km"+conversionHistory;
-            history.setText(conversionHistory);
+            Double inputValueInNumber = Double.parseDouble(inputValue.getText().toString());
+            Double output;
+            if(radioInput.equals("Kilometers to Miles")){
+                Log.d(TAG, "Input in Kilometers: "+inputValueInNumber);
+                output = (inputValueInNumber)*0.621371;
+                output = Math.round(output*10.0)/10.0;
+                conversionHistory = System.getProperty("line.separator")+"     "+inputValueInNumber.toString()+"  Km ==>  "+output.toString()+"  Mi"+conversionHistory;
+                history.setText(conversionHistory);
+                finalResult.setText("  "+output.toString()+"   miles");
+                inputValue.setText("");
+            }else {
+                Log.d(TAG, "Input in Miles: "+inputValueInNumber);
+                output = (inputValueInNumber)*1.60934;
+                output = Math.round(output*10.0)/10.0;
+                finalResult.setText("  "+output.toString()+"   kilometers");
+                conversionHistory =System.getProperty("line.separator")+"     "+inputValueInNumber.toString()+"  Mi  ==>  "+output.toString()+"  Km"+conversionHistory;
+                history.setText(conversionHistory);
+                inputValue.setText("");
+            }
         }
     }
 
@@ -120,6 +148,11 @@ public class MainActivity extends AppCompatActivity {
 
         outState.putString("ConversionHistory", history.getText().toString());
         outState.putString("ConvertedResult", finalResult.getText().toString());
+        outState.putString("InputValue",inputValue.getText().toString());
+        outState.putString("InputValueHeading", inputHeading.getText().toString());
+        outState.putString("OutputHeading", outputHeading.getText().toString());
+        outState.putString("ButtonText",buttonText.getText().toString());
+        outState.putString("InputValueHint",inputValue.getHint().toString());
         super.onSaveInstanceState(outState);
     }
 
@@ -127,7 +160,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
 
         super.onRestoreInstanceState(savedInstanceState);
+        inputValue.setText(savedInstanceState.getString("InputValue"));
+        conversionHistory = savedInstanceState.getString("ConversionHistory");
         history.setText(savedInstanceState.getString("ConversionHistory"));
         finalResult.setText(savedInstanceState.getString("ConvertedResult"));
+        inputHeading.setText(savedInstanceState.getString("InputValueHeading"));
+        outputHeading.setText(savedInstanceState.getString("OutputHeading"));
+        buttonText.setText(savedInstanceState.getString("ButtonText"));
+        inputValue.setHint(savedInstanceState.getString("InputValueHint"));
     }
 }
